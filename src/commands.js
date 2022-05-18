ace69c72ed826e0153e6135ab58416f88b7fa277
Pink-Hat-Hacker <zyv@udel.edu>
2022-05-18 18:36:10 -0400

    using fkcodes work

---
+++ src/commands.js
@@ -0,0 +1,79 @@
+var photolio = "https://www.zoevalladares.com";
+var instagram = "https://www.instagram.com/illchange_this_later/";
+var github = "https://github.com/pink-hat-hacker/";
+var email = 'mailto:zyv@udel.edu';
+
+whois = [
+  "<br>",
+  "Hey, Pink Hat Hacker (aka Zoe Valladares)! 🎩",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'photolio       <a href="' + photolio + '" target="_blank">photolio/zoevalladares' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/illchange_this_later' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/pink-hat-hacker' + "</a>",
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Pink Hat Hacker?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           duh',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+    '<span class="index">Pink-Hat-Hacker (PHH) Not A Corporation. All hats reserved.</span>',
+    "        .e$$$$$$$$$o.  .z+^^-eu.      .xu..",
+    "     d$$$$$$$$$$$$$$$$$o.    $$$$$u#         ^%s",
+    "   u$$$$$$$$$$$$$$$$$$$$$$.  $$$$^              ^s           ___                   ___          ___             ",
+    "  u$$$$$$$$$$$$$$$$$$$$$$$$u $$$                  ^.        /\\  \\        ___      /\\__\\        /\\__\\      ",
+    "  $$$$$$$$$$$$$$$$$$$$$$$$$$c$$                    'c      /::\\  \\      /\\  \\    /::|  |      /:/  /        ",
+    "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$               :       k     /:/\\:\\  \\     \\:\\  \\  /:|:|  |     /:/__/       ",
+    " $$$$$$$$$$$$$$$$$$$$$$$$$$$$$              :z!      '>   /::\\~\\:\\  \\    /::\\__\\/:/|:|  |__  /::\\__\\____",
+    "$$$$$$$$$$$$$$$$$$$$$$$$$$$$EB             R         5  /:/\\:\\ \\:\\__\\__/:/\\/__/:/ |:| /\\__\\/:/\\:::::\\__\\",
+    "$$$$$$$$^RR$$$$$$$$$$$$$$$$$E'            :E         9  \\/__\\:\\/:/  /\\/:/  /  \\/__|:|/:/  /\\/_|:|~~|~",
+    " $$$$$$P   $$$$$$$$$$$$$$$$$$E k           ' `%       F       \\::/  /\\::/__/       |:/:/  /    |:|  |",
+    "$$$$$&    9$$$$$$$$$$$$$$$$$E ?           '   *     z         \\/__/  \\:\\__\\       |::/  /     |:|  |",
+    " $$$$      9$$$$$$$$$$$$$$$$$`  $           >   k   X                  \\/__/       /:/  /      |:|  |",
+    " d**Nx     $$$$$$$$$$$$$$$$$$   ''          5   ?  f                               \\/__/        \\|__|",
+    "$$  ?Lk   :$$$$$$$$$$$$$$$$$$               `  u#$$N",
+    "RRF $5^   8$$$$$$$$$$$$$$$$$$                E?&9$$FE",
+    " ^**#`    $$$$$$$$$$$$$$$$$$$                ? ^$$$@`",
+    "         J$*$$$$$$$$$$$$$$$$$                 L",
+    "        x$$$$c#$$$$**#^^^^^^^$$$$$$$$$$$$eed^R",
+    '<span class="color2">Welcome to my interactive web terminal.</span>',
+    "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
+
+
+
+
+
+
+
+
+ 
+  
+
